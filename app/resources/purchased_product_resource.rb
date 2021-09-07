78bb69e851d042b35ae100d65f3423e4ab203c0b
anonymous anonymous <wsun3@uchicago.edu>
2021-09-07 02:43:16 +0000

    Generate graphiti resources for purchased_products

---
+++ app/resources/purchased_product_resource.rb
@@ -0,0 +1,14 @@
+class PurchasedProductResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :products_id, :integer
+  attribute :coupons_id, :integer
+  attribute :customers_id, :integer
+  attribute :quantity, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
