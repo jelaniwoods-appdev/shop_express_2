2d83233a5e2a3e8619dafe397b9ecbea4058a10a
anonymous anonymous <wsun3@uchicago.edu>
2021-09-07 02:41:15 +0000

    Generate purchased_products resource with ideas:scaffold

---
+++ db/migrate/20210907024115_create_purchased_products.rb
@@ -0,0 +1,12 @@
+class CreatePurchasedProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :purchased_products do |t|
+      t.integer :products_id
+      t.integer :coupons_id
+      t.integer :customers_id
+      t.string :quantity
+
+      t.timestamps
+    end
+  end
+end
