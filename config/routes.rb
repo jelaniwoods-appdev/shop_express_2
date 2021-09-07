cc387dde9fc46e625201210b5485d1c36ee90dab
anonymous anonymous <wsun3@uchicago.edu>
2021-09-07 02:41:42 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "coupons#index"
   resources :assigned_coupons
   resources :customer_friends
